8df4fdb385bb45f221542121dad84f35ce6e5589
Oliver Volpis <silva.oliver@escola.pr.gov.br>
2025-08-20 10:34:16 -0300

    Update sistemabancario.java

--- sistemabancario.java
+++ sistemabancario.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class SistemaBancario {
+  public static void main (String[] args) {
+    
+    int escolha = 0;
+     System.out.print("""
+      Operações:
+      -------------------------
+      | (1) Consultar Saldo   |
+      | (2) Realizar Depósito |
+      | (3) Realizar Saque    |
+      | (4) Sair              |
+      -------------------------
+     """); 
+     
+    System.out.println("\nQual sua opção : ");
+    Scanner opcao = new Scanner (System.in);
+    escolha = opcao.nextInt(0);
+  }
+  
+}
